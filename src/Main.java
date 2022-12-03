e80e105b9826e5f9ae346932864f911735d38bf9
Popov Evgeniy <evgenij.selin.94@bk.ru>
2022-12-03 16:34:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public
+class Main {
+    public static
+    void main(String[] args) {
+
+        int ticket = 100_000;
+        int miles = 20;
+
+        System.out.println("количество бонусных милей ");
+        System.out.println((ticket / miles) + " бонусных милей");
+
+        int bonus = 5_000;
+    }
+}
